b7fcdbfdaa613c940cf6069e35076ad9dc1c7c80
stevecyj <ycsteven.tsao@gmail.com>
2023-11-18 19:42:28 +0800

    feat: add Koa server with GET route for /test
    - Added a new file `src/app.ts` with code for a Koa server
    - Imported `Koa`, `koa-body`, `koa-json`, and `koa-router` modules
    - Created a new instance of `Koa` and `Router`
    - Set the router prefix to `/dang`
    - Added a GET route for `/test` that returns "hello world"
    - Added middleware for JSON parsing and request body parsing
    - Configured the app to use the router and listen on port 3002
    - Printed a message indicating that the server is running at http://localhost:3002
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import Koa from "koa";
+import body from "koa-body";
+import json from "koa-json";
+import Router from "koa-router";
+
+const app = new Koa();
+const router = new Router();
+router.prefix("/dang"); // 路由前缀
+
+router.get("/test", async (ctx: Koa.Context) => {
+  ctx.body = "hello world";
+});
+router.use(json());
+router.use(body());
+
+app.use(router.routes());
+app.listen(3002);
+console.log("server is running at http://localhost:3002");
